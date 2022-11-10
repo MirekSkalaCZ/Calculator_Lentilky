b7a81db4a9da7807309c9cfb51046073c7c94be8
Mirek Skala <mirek.skala@seznam.cz>
2022-11-10 16:49:03 +0100

    Calculator Design Done

---
+++ script.js
@@ -0,0 +1,21 @@
+/**** Variables ****/
+
+/* Display */
+
+let display = document.getElementById("display");
+
+/* Number Buttons */
+
+const btn_null = document.getElementById("btn_0");
+const btn_one = document.getElementById("btn_1");
+const btn_two = document.getElementById("btn_2");
+const btn_three = document.getElementById("btn_3");
+const btn_four = document.getElementById("btn_4");
+const btn_five = document.getElementById("btn_5");
+const btn_six = document.getElementById("btn_6");
+const btn_seven = document.getElementById("btn_7");
+const btn_eight = document.getElementById("btn_8");
+const btn_nine = document.getElementById("btn_9");
+
+/* Neumber Math Symbols */
+
