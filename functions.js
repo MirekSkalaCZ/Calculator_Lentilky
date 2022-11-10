e917a12a2011a2cb2bab8e9e1c7026ddd49df6ef
Mirek Skala <mirek.skala@seznam.cz>
2022-11-10 22:07:41 +0100

    Final version

--- functions.js
+++ functions.js
@@ -4,11 +4,87 @@
 
 let display = document.getElementById("display");
 
+/** Buttons Variables **/
+
+let btn_divide = document.getElementById("btn_divide");
+
+let btn_power = document.getElementById("btn_power");
+
+let btn_times = document.getElementById("btn_times");
+
+let btn_square = document.getElementById("btn_square");
+
 let btn_reset = document.getElementById("btn_reset");
+
+let btn_percent = document.getElementById("btn_percent");
 
 let btn_equal = document.getElementById("btn_equal");
 
 /**** Functions ****/
+
+/** Other Functions **/
+
+/* Replacing values */
+
+function zeroing(){
+
+    if(display.value == 0){
+        display.value = "";
+    }
+
+}
+
+zeroing();
+
+/** Buttons Functions **/
+
+/* Divide Function */
+
+function divide(){
+
+    btn_divide.addEventListener("click", function(){
+        display.value += "/";
+    })
+
+}
+
+divide();
+
+/* Power Function */
+
+function power(){
+
+    btn_power.addEventListener("click", function(){
+        display.value = Math.pow(display.value, 2).toFixed(2);
+    })
+
+}
+
+power();
+
+/* Times Function */
+
+function times(){
+
+    btn_times.addEventListener("click", function(){
+        display.value += "*";
+    })
+
+}
+
+times();
+
+/* Square Function */
+
+function square(){
+
+    btn_square.addEventListener("click", function(){
+        display.value = Math.sqrt(display.value).toFixed(2);
+    })
+
+}
+
+square();
 
 /* Reset Function */
 
@@ -20,6 +96,20 @@ function reset(){
 
 }
 
+reset();
+
+/* Percent Function */
+
+function percent(){
+
+    btn_percent.addEventListener("click", function(){
+        display.value = eval(display.value / 100);
+    })
+
+}
+
+percent();
+
 /* Equal Function */
 
 function equal(){
@@ -30,13 +120,8 @@ function equal(){
 
 }
 
-/* Replacing values */
+equal();
 
-function zeroing(){
 
-    if(display.value == 0){
-        display.value = "";
-    }
 
-}
 
--- script.js
+++ script.js
@@ -34,11 +34,6 @@ for(let i = 0; i <= 2; i++){
     
 }
 
-/* Calling Functions */
-
-reset();
-
-equal();
 
 
 
